67016ed521d02386d871d127602ef2df5bb379f9
Edrich0902 <edrichbarnard@gmail.com>
2020-03-31 23:51:03 +0200

    Version 1 of the login screen. Changes are still necessary, but the layout and design is done. Still requires a lot of JavaSript for input validation

---
+++ WebPortal/Script/script.js
@@ -0,0 +1,9 @@
+/**
+ * The following script changes the password input field from text to password
+ * when clicked in order to hide the password.
+ * @return {undefined}
+ */
+function changeType()
+{
+    document.getElementById("pw").type = "password";
+}
--- WebPortal/index.php
+++ WebPortal/index.php
@@ -3,11 +3,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Compulink Technologies</title>
+        <!-- Displays Compulink logo in title bar of web pages -->
+        <link rel="shortcut icon" href="images/logo_block__1__AHI_icon.ico" />
+        <!-- Link external CSS to the main Index page -->
+        <link href="CSS/style.css" rel="stylesheet" type="text/css"/>
+        <script src="Script/script.js" type="text/javascript"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="mainPage">
+            <div class="loginBox">
+                <div class="loginBoxTitleContent">
+                    <img src="images/logo_block_cropped.png" alt="" class="compuLogo"/><br/><br/>
+                    <p class="title">Compulink Technologies</p>
+                </div>
+                <div class="loginBoxFormContent">
+                    <input type="submit" value="Login" name="Login" class="loginBut"/> <input type="submit" value="Register" name="Register" class="regBut"/><br/><br/>
+                    <input type="text" name="Username" value="Username" class="userInp" onfocus="this.value=''"/><br/><br/>
+                    <input type="text" name="Password" value="Password" class="pwInp" onfocus="this.value=''; changeType()" id="pw"/><br/><br/>
+                    <input type="submit" value="Login" name="LoginSubmit" class="loginSubBut"/>
+                    <p><a href="#" class="forgotPw">Forgot password?</a></p>
+                    <p><a href="#" class="regMsg">Register an account</a></p>
+                </div>
+            </div>
+            <div class="footer">
+                Linkvantage &copy; 2020
+            </div>
+        </div>
     </body>
 </html>
