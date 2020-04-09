2e553b021a310c4bcace5c04a261d9d2d6a75619
Edrich0902 <edrichbarnard@gmail.com>
2020-04-09 14:31:34 +0200

    The second page was completed. The forgot password and login pages are now both done. With a transition between them

--- WebPortal/Script/script.js
+++ WebPortal/Script/script.js
@@ -1,4 +1,10 @@
 /**
+ * The necessary logic and if statements will be added to the 
+ * javascript at a later stage.
+ * @return {undefined}
+ */
+
+/**
  * The following script changes the password input field from text to password
  * when clicked in order to hide the password.
  * @return {undefined}
@@ -6,4 +12,31 @@
 function changeType()
 {
     document.getElementById("passw").type = "password";
+}
+
+/**
+ * This function changes the html displayed and hides the main login page 
+ * and displays the forgot password page
+ * @return {undefined}
+ */
+function forgotPasswordPage()
+{
+    var div = document.getElementById("loginPageMain");
+    div.style.display = "none";
+    
+    var div2 = document.getElementById("forgotPasswordPage");
+    div2.style.display = "block";
+}
+
+/**
+ * changes from the reset password page to the login page
+ * @return {undefined}
+ */
+function changeToLogin()
+{
+    var div = document.getElementById("forgotPasswordPage");
+    div.style.display = "none";
+    
+    var div2 = document.getElementById("loginPageMain");
+    div2.style.display = "block";
 }
--- WebPortal/index.php
+++ WebPortal/index.php
@@ -8,10 +8,12 @@
         <link rel="shortcut icon" href="images/logo_block__1__AHI_icon.ico" />
         <!-- Link external CSS to the main Index page -->
         <link href="CSS/style.css" rel="stylesheet" type="text/css"/>
+        <link href="CSS/forgotPw.css" rel="stylesheet" type="text/css"/>
+        <!--All javascript-->
         <script src="Script/script.js" type="text/javascript"></script>
     </head>
     <body>
-        <div class="mainPage">
+        <div class="loginPage" id="loginPageMain">
             <div class="header">
                 Compulink Technologies
             </div>
@@ -31,11 +33,49 @@
                     <input type="text" name="username" placeholder="USERNAME" class="input" id="username"/>
                     <img src="images/lock.png" alt="" class="lockImg"/>
                     <input type="text" name="password" placeholder="PASSWORD" class="input" id="passw" onfocus="changeType()"/>
-                    <font class="forgotPw"><a href="#">Forgot password?</a></font>
+                    <font class="forgotPw"><a href="#" onclick="forgotPasswordPage()">Forgot password?</a></font>
                 </div>
                 
                 <div class="loginSubmit">
                     <input type="submit" value="LOGIN" name="loginSub" class="loginSubBtn"/>
+                </div>
+            </div>
+            
+            <div class="footer">
+                Linkvantage
+            </div>
+        </div>
+        
+        
+        
+        <div class="forgotPasswordPage" id="forgotPasswordPage">
+            <div class="header">
+                Compulink Technologies
+            </div>
+            
+            <div class="contentFgPw">
+                <div class="logo">
+                    <img src="images/logo_block_cropped.png" alt="" id="compuLogo"/>
+                </div>
+                
+                <div class="backToLogin">
+                    <input type="submit" value="RETURN TO LOGIN" name="returnSub" class="returnToLoginBtn" onclick="changeToLogin()"/>
+                </div>
+                
+                <div class="resetInpContent">
+                    <img src="images/refresh.png" alt="" class="resetImg"/>
+                    <select name="resetOptions" class="dropDownSelect">
+                        <option>TEXT OTP</option>
+                        <option>ANDROID OTP</option>
+                        <option>RESET PASSWORD</option>
+                    </select>
+                    
+                    <img src="images/envelope.png" alt="" class="emailImg"/>
+                    <input type="text" name="emailInp" value="" placeholder="EMAIL" class="emailInp"/>
+                </div>
+                
+                <div class="resetSubmit">
+                    <input type="submit" value="SEND RESET REQUEST" name="resetSub" class="resetSubBtn"/>
                 </div>
             </div>
             
