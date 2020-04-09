f7b6dc7471a43879088cbd8ec9461fea8110b1ad
Edrich0902 <edrichbarnard@gmail.com>
2020-04-09 19:30:41 +0200

    added content to register page

--- WebPortal/index.php
+++ WebPortal/index.php
@@ -117,7 +117,45 @@
                 </div>
             </div>  
             
-            <div class="footer">
+            <div class="contactsHeader">
+                <font>Contacts</font>
+            </div>
+            
+            <div class="contactsContent">
+                <div class="contactContentHeader">
+                    <img src="images/cross.png" alt="" id="contactCrossImg" class="contactCrossImg"/>
+                    Contact <font id="number"> 1</font><hr class="contactContentHeaderLine">
+                    <img src="images/down-arrow.png" alt="" id="contactDownArrow" class="contactDownArrow"/>
+                </div>
+                
+                <div class="usernPassInp">
+                    <img src="images/account.png" alt="" class="contactAccountImg"/>
+                    <input type="text" name="username" placeholder="USERNAME" class="contactInput" id="ContactUsername"/>
+                    <img src="images/lock.png" alt="" class="contactLockImg"/>
+                    <input type="text" name="password" placeholder="PASSWORD" class="contactInput" id="ContactPassw" onfocus="changeType()"/>
+                </div>
+                
+                <div class="names">
+                    <img src="images/id-card.png" alt="" class="idCard" id="id1"/>
+                    <input type="text" name="firstName" placeholder="FIRST NAME" class="contactInput" id="contactFirstName"/>
+                    <img src="images/id-card.png" alt="" class="idCard"/>
+                    <input type="text" name="lastName" placeholder="LAST NAME" class="contactInput" id="contactLastName"/>
+                </div>
+                
+                <div class="otherContactDet">
+                    <img src="images/envelope.png" alt="" class="contactEmailImg"/>
+                    <input type="text" name="email" placeholder="EMAIL" class="contactInput" id="contactEmail"/>
+                    <img src="images/phone.png" alt="" class='phoneContactImg'/>
+                    <input type="text" name="phoneNumber" placeholder="PHONE NUMBER" class="contactInput" id="contactPhoneNum"/>
+                </div>
+
+                <div class="confirmContact">
+                    <label for="confirmMainContact">Is the main contact:</label>
+                    <input type="checkbox" name="confirmMainContact" value="ON" checked="checked" id="confirmMainContact"/>
+                </div>
+            </div>
+            
+            <div class="registerFooter">
                 Linkvantage
             </div>
         </div>
