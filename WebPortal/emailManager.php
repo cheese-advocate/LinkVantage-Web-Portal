2ae91f73d409cf15ff6b2bc568b73be0192f8def
Bernard_G <bernard01geldenhuys@gmail.com>
2020-04-24 14:21:39 +0200

    phpEmailer successfully implemented. Able to send emails from any gmail account with password and username. php.ini file modified sendmail.ini file modified emailManager class not finished

--- WebPortal/emailManager.php
+++ WebPortal/emailManager.php
@@ -1,13 +1,27 @@
 <?php
 
-$to = "bernard01geldenhuys@gmail.com";
-$subject = "chai";
-$message = "<h1>CHAI</h1>";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$headers = "From: Mr Chai <drchai101>\r\n";
-$headers .= "Reply-to: drchai101\r\n";
-$headers .= "Content-type: text/html\r\n";
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
 
-mail($to, $subject, $message, $headers);
+$recEmail = "customer@gmail.com";
 
-// Tristan says Hi
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+$mail->SMTPDebug = 0; //shows debugging information. 0 shows nothing.
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = "587"; // typically 587 
+$mail->SMTPSecure = 'tls'; 
+$mail->SMTPAuth = true;
+$mail->Username = "drchai101@gmail.com"; //your email address
+$mail->Password = "thisischai"; //password for email acocunt
+$mail->setFrom("drchai101@gmail.com", "Dr Chai");
+$mail->addAddress($recEmail, "Chai Consortium");
+$mail->Subject = 'CHAI';
+$mail->msgHTML("CHAI");
+
+$mail->send();
+
