4299b582a0edfc9f5dd48929622369ac1494770f
Bernard_G <bernard01geldenhuys@gmail.com>
2020-04-24 08:12:05 +0200

    Still testing mail function

--- WebPortal/emailManager.php
+++ WebPortal/emailManager.php
@@ -1,4 +1,11 @@
 <?php
 
-echo "test";
+$to = "bernard01geldenhuys@gmail.com";
+$subject = "chai";
+$message = "<h1>CHAI</h1>";
 
+$headers = "From: Mr Chai <drchai101>\r\n";
+$headers .= "Reply-to: drchai101\r\n";
+$headers .= "Content-type: text/html\r\n";
+
+mail($to, $subject, $message, $headers);
