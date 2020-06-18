006e94bbc9f0170d76b53df66916b6d5fcd6b8f0
Edrich0902 <edrichbarnard@gmail.com>
2020-06-18 16:24:28 +0200

    Added a dashboard. Navigation from login to dashboard is now possible.

---
+++ WebPortal/Dashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Compulink Technologies</title>
+        <!--Title icon link-->
+        <link rel="shortcut icon" href="favicon.ico" />
+        <!--CSS links-->
+        <link href="CSS/style.css" rel="stylesheet" type="text/css"/>
+        <link href="CSS/dashboard.css" rel="stylesheet" type="text/css"/>
+        <!--JS Links-->
+        <script src="Script/dashboard.js" type="text/javascript"></script>
+    </head>
+    <body>        
+        <div class="mainPage">
+            <div class="topNav">
+                <img src="images/homeLink.png" alt="" class="homeLink"/>
+                <div class="userInfo">
+                    <span class="userName"></span>
+                    <img src="images/account.png" alt="" class="account"/>
+                </div>
+            </div>
+
+            <div class="sideNav">
+                
+            </div>
+        </div>
+    </body>
+</html>
